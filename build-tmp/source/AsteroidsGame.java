a565643bf526e09e00b508f2efeaa0f43e9546c0
minataur <minac.98sf@gmail.com>
2014-11-20 10:51:41 -0800

    Stars and moving

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip suzy;  
-int count = 10AZ0;
+int count = 50;
 Star[] alice =  new Star[count];
 
 
@@ -23,8 +39,8 @@ public void draw() {
 }
 
 public void keyPressed() {
-  if (keyCode==UP) {suzy.accelerate(0.1);}
-  else if (keyCode==DOWN) {suzy.accelerate(-0.1);}
+  if (keyCode==UP) {suzy.accelerate(0.1f);}
+  else if (keyCode==DOWN) {suzy.accelerate(-0.1f);}
   else if (keyCode==RIGHT) {suzy.rotate(20);}
   else if (keyCode==LEFT) {suzy.rotate(-20);}
 }
@@ -41,7 +57,7 @@ class Star {
     noStroke();
     fill(255);
     float angle = TWO_PI / 5;
-    float halfAngle = angle/2.0;
+    float halfAngle = angle/2.0f;
     beginShape();
     for (float a = 0; a < TWO_PI; a += angle) {
       float sx = myX + cos(a) * 2;
@@ -158,3 +174,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   }   
 } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
